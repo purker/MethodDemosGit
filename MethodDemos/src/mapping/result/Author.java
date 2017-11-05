119b5488b28b4db16259f69c242f3aa080ae2c3c
Angela <purker_angela@hotmail.com>
2017-11-05 23:17:10 +0100

    Author.firstName to firstNames geändert

--- MethodDemos/src/mapping/result/Author.java
+++ MethodDemos/src/mapping/result/Author.java
@@ -1,11 +1,13 @@
 package mapping.result;
 
+import java.util.List;
+
 public class Author extends BaseEntity
 {
 	private static final long serialVersionUID = -7444267954223367935L;
 
-	protected String name; // if null, information in first and lastname
-	protected String firstName;
+	private String name; // if null, information in first and lastname
+	protected List<String> firstNames;
 	protected String lastName;
 
 	protected String email;
@@ -13,29 +15,19 @@ public class Author extends BaseEntity
 	protected String type;
 	protected Affiliation affiliation;
 
-	public String getName()
-	{
-		return name;
-	}
-
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-
-	public String getFirstName()
-	{
-		return firstName;
-	}
-
-	public void setFirstName(String firstName)
-	{
-		this.firstName = firstName;
-	}
-
 	public String getLastName()
 	{
 		return lastName;
+	}
+
+	public List<String> getFirstNames()
+	{
+		return firstNames;
+	}
+
+	public void setFirstNames(List<String> firstNames)
+	{
+		this.firstNames = firstNames;
 	}
 
 	public void setLastName(String lastName)
@@ -51,16 +43,6 @@ public class Author extends BaseEntity
 	public void setEmail(String email)
 	{
 		this.email = email;
-	}
-
-	public String getType()
-	{
-		return type;
-	}
-
-	public void setType(String type)
-	{
-		this.type = type;
 	}
 
 	public Affiliation getAffiliation()
