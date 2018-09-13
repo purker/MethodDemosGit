58e0300213900946840b312d42b815afa3f1e0dd
Angela <purker_angela@hotmail.com>
2018-09-13 16:44:17 +0200

    SquareBracketNameComaYearMarkerStyle

---
+++ MethodDemos/src/mapping/markers/SquareBracketNameComaYearMarkerStyle.java
@@ -0,0 +1,49 @@
+package mapping.markers;
+
+import mapping.result.Reference;
+import utils.CollectionUtil;
+import utils.PublicationUtil;
+
+/**
+ * 1: [Miksch, 1999]<br>
+ * 2: [Lee and Geller, 2006]<br>
+ * >2: [Peleg et al., 2003]
+ */
+public class SquareBracketNameComaYearMarkerStyle extends AbstractMarkerStyle
+{
+
+	@Override
+	public String getMarkerString(Reference reference)
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append("[");
+		if(CollectionUtil.isNotEmpty(reference.getAuthors()))
+		{
+			sb.append(PublicationUtil.getConcatinatedLastNamesOfAuthors(reference, " and "));
+		}
+		else
+		{
+			if(reference.getPublisher() != null)
+			{
+				sb.append(reference.getPublisher());
+			}
+			else
+				if(reference.getEditors() != null)
+				{
+					sb.append(reference.getEditors());
+				}
+		}
+		sb.append(", ");
+		if(reference.getNote() != null)
+		{
+			sb.append(reference.getNote());
+		}
+		else
+		{
+			sb.append(reference.getPublicationYear());
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
+}
